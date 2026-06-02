0aad1e74e121fdd4e9ac14f2fec31a3df47f19ca
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-06-02 16:44:08 -0400

    add volume

--- worker/extractor.py
+++ worker/extractor.py
@@ -9,7 +9,7 @@ def extract_clips(job_id: int, top_clips: list[dict], source_path: str, db) -> N
         output_file = f"/app/clips/{job_id}/clip_{i+1}.mp4"
 
 
-        ffmpeg.input(input_file, ss=clip["start_second"], to=clip["end_second"]).output(output_file, codec='copy').run()
+        ffmpeg.input(input_file, ss=clip["start_second"], to=clip["end_second"]).output(output_file, af="volume-1.5", vcodec="copy", acodec="aac").run()
         final_clip = Clip(
             job_id=job_id,
             file_path=output_file,
