129ea7d7e4933a358f710ea772a3d33f43c344d3
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-06-03 17:15:09 -0400

    add volume

--- worker/extractor.py
+++ worker/extractor.py
@@ -14,7 +14,9 @@ def extract_clips(job_id: int, top_clips: list[dict], source_path: str, db) -> N
         output_file = f"/app/clips/{job_id}/clip_{i+1}.mp4"
 
 
-        ffmpeg.input(input_file, ss=clip["start_second"], to=clip["end_second"]).output(output_file, af="volume=1.5", vcodec="copy", acodec="aac").run()
+        stream = ffmpeg.input(input_file, ss=clip["start_second"], to=clip["end_second"])
+        audio = stream.audio.filter("volume", 1.5)
+        ffmpeg.output(stream.video, audio, output_file, vcodec="copy", acodec="aac").run()
         gcs_url = upload_public_file(bucket_name=os.getenv("GCS_BUCKET_NAME"), source_file_path=output_file, destination_blob_name=f"{job_id}/clip_{i+1}.mp4")
         final_clip = Clip(
             job_id=job_id,
